80c610da40f851f22b7a0f61f4055669d28c8fb7
Jianhao Geng <g654602355@gmail.com>
2023-10-23 17:27:32 +0800

    老人数量算法
    给你一个下标从 0 开始的字符串 details 。details 中每个元素都是一位乘客的信息，信息用长度为 15 的字符串表示，表示方式如下：
    
    前十个字符是乘客的手机号码。
    接下来的一个字符是乘客的性别。
    接下来两个字符是乘客的年龄。
    最后两个字符是乘客的座位号。
    请你返回乘客中年龄 严格大于 60 岁 的人数
    
    例如：
    输入：details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
    输出：2
    解释：下标为 0 ，1 和 2 的乘客年龄分别为 75 ，92 和 40 。所以有 2 人年龄大于 60 岁。

--- phpver.php
+++ phpver.php
@@ -1,2 +1,19 @@
 <?php
-  
+    /**
+     * 老人的数量
+     * @param string[] $details
+     * @param int $year
+     * @return int
+     * @author jianhaofly@163.com
+     * @time 2023/10/23 17:24
+     */
+    function countSeniors($details, $year) {
+        $num = 0;
+        foreach ($details as $detail) {
+            $nowYear = intval($detail[11].$detail[12]);
+            if ($nowYear > $year) {
+                $num++;
+            }
+        }
+        return $num;
+    }
