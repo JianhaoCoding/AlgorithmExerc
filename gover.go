009a83acda5918cf62febce52bc36ef7e7eda02e
Jianhao Geng <g654602355@gmail.com>
2023-10-23 17:34:04 +0800

    老人数量算法go版本

--- gover.go
+++ gover.go
@@ -1,1 +1,23 @@
+package main
 
+import {
+  "fmt"
+	"strconv"
+}
+
+func main() {
+    details := []string{"7868190130M7522", "5303914400F9211", "9273338290F4010"}
+	  fmt.Println(countSeniors(details))
+}
+
+// 老人的数量
+func countSeniors(details []string, age int) int {
+    count := 0
+    for _, detail = range details {
+      nowAge, err = strconv.Atoi(detail[11, 13])
+      if err == nil && nowAge >= age {
+          count++
+      }
+    }
+    return count
+}
